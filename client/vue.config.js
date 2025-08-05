2038f1a3ff36c943bab7d0432d19921ec28dbf4a
eid <group12.hailvr@gmail.com>
2025-08-05 09:29:12 +0300

    Update vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: path.resolve(__dirname, "../server/public"),
+  outputDir: path.resolve(__dirname, "/public"),
   publicPath: "./",
   devServer: {
     proxy: {

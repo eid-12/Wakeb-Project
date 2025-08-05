05007736f9a9297f0c50978842d44fd7d07b2dbb
eid <group12.hailvr@gmail.com>
2025-08-05 11:40:46 +0300

    a
    a

--- client/server.js
+++ client/server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Frontend running on port ${port}`);
-});
+app.listen(port, () => console.log(`Frontend running on ${port}`));
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,15 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: path.resolve(__dirname, "/public"),
+  outputDir: path.resolve(__dirname, "dist"),
   publicPath: "./",
   devServer: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
+    proxy: { "/api": { target: "http://localhost:3000", changeOrigin: true } }
+  }
 });
